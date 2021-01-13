1f516798e5e662e601c917114682391949908314
Thi <dinhanhthi@gmail.com>
2021-01-13 20:33:00 +0100

    formatting

--- index.js
+++ index.js
@@ -13,46 +13,44 @@ let goodImg = true;
 
 // init
 async function initialize() {
-		// 193MB version
-    // model = await tf.loadGraphModel('saved_model/not-quantized/model.json');
+	// 193MB version
+	// model = await tf.loadGraphModel('saved_model/not-quantized/model.json');
 
-		// 48MB version
-		model = await tf.loadGraphModel('saved_model/quantized/model.json');
-		classifierElement.style.display = 'block';
-		listImgs.style.display = "flex";
-		if (bar<99){
-			bar = 100;
-			clearInterval(idloader);
-			loader.style.display = "none";
-		}
-    document.getElementById('predict').addEventListener('click', () => {
-			predict();
-		});
-}
-
-async function fetch_img_url(){
-	await fetch(proxyUrl+inputURL.value) // https://cors-anywhere.herokuapp.com/${url}
-	.then(response => response.blob())
-	.then(images => {
-		imageDisplay.src = URL.createObjectURL(images);
+	// 48MB version
+	model = await tf.loadGraphModel('saved_model/quantized/model.json');
+	classifierElement.style.display = 'block';
+	listImgs.style.display = "flex";
+	if (bar < 99) {
+		bar = 100;
+		clearInterval(idloader);
+		loader.style.display = "none";
+	}
+	document.getElementById('predict').addEventListener('click', () => {
+		predict();
 	});
 }
 
+async function fetch_img_url() {
+	await fetch(proxyUrl + inputURL.value) // https://cors-anywhere.herokuapp.com/${url}
+		.then(response => response.blob())
+		.then(images => {
+			imageDisplay.src = URL.createObjectURL(images);
+		});
+}
+
 // when predict button is clicked
-async function predict () {
+async function predict() {
 	try {
-		if (imageDisplay.src != ""){
+		if (imageDisplay.src != "") {
 
 			mainImage.style.display = "block";
 
 			const offset = tf.scalar(255.0);
-			let tensorImg = tf.browser
-												.fromPixels(imageDisplay)
-												// .fromPixels(clonedImgDisp)
-												.resizeNearestNeighbor([200, 200])
-												.toFloat().sub(offset)
-												.div(offset)
-												.expandDims();
+			let tensorImg = tf.browser.fromPixels(imageDisplay)
+				.resizeNearestNeighbor([200, 200])
+				.toFloat().sub(offset)
+				.div(offset)
+				.expandDims();
 			prediction = await model.predict(tensorImg).data();
 
 			result.style.display = "block";
@@ -64,19 +62,19 @@ async function predict () {
 				result.getElementsByTagName("p")[0].innerHTML
 					= "It's <b>a mountain</b> (or related)!"
 					+ " I'm <b>" + String(score) + "%</b> sure!";
-			} else if(prediction[0] < 1-threshold) {
+			} else if (prediction[0] < 1 - threshold) {
 				result.getElementsByTagName("p")[0].innerHTML
 					= "It's <b>a beach</b> (or related)!"
-						+ " I'm <b>" + String(100-score) + "%</b> sure!";
-			} else{
-				var notSure = ((prediction[0]>0.5) ? "mountain" : "beach");
-				var notSureScore = ((prediction[0]>0.5) ? score : 100-score);
+					+ " I'm <b>" + String(100 - score) + "%</b> sure!";
+			} else {
+				var notSure = ((prediction[0] > 0.5) ? "mountain" : "beach");
+				var notSureScore = ((prediction[0] > 0.5) ? score : 100 - score);
 				result.getElementsByTagName("p")[0].innerHTML
 					= "I'm not so confident what it is!"
-						+ " There may be some errors with your image or the image you indicated is not clearly a mountain or a beach."
-						+ " Please choose another one."
-						+ " If you ask me to guess anyway, it's a <b>" + notSure
-						+ "</b> with <b>" + String(notSureScore) + "%</b> confidence!";
+					+ " There may be some errors with your image or the image you indicated is not clearly a mountain or a beach."
+					+ " Please choose another one."
+					+ " If you ask me to guess anyway, it's a <b>" + notSure
+					+ "</b> with <b>" + String(notSureScore) + "%</b> confidence!";
 			}
 
 		} else {
@@ -91,36 +89,36 @@ async function predict () {
 }
 
 // cannot load image
-function handleErrImg(){
+function handleErrImg() {
 	goodImg = false;
 }
 
 // loading bar
-let seconds = 40;
+let seconds = 50;
 let bar = 1;
-let idloader = setInterval(function(){
-    bar++;
-		var percent = loader.lastElementChild.firstElementChild;
-		percent.setAttribute("aria-valuenow", String(bar));
-		percent.style = "width: " + String(bar) + "%;";
+let idloader = setInterval(function () {
+	bar++;
+	var percent = loader.lastElementChild.firstElementChild;
+	percent.setAttribute("aria-valuenow", String(bar));
+	percent.style = "width: " + String(bar) + "%;";
 }, seconds * 1000 / 100)
 
 // button upload
 function changeImage() {
-    var uploadedImage = document.getElementById('my-file-selector').files[0];
-		imageDisplay.src = URL.createObjectURL(uploadedImage);
-		result.style.display = "none";
-		mainImage.style.display = "block";
-		imgTextPredict.style.display = "none";
+	var uploadedImage = document.getElementById('my-file-selector').files[0];
+	imageDisplay.src = URL.createObjectURL(uploadedImage);
+	result.style.display = "none";
+	mainImage.style.display = "block";
+	imgTextPredict.style.display = "none";
 }
 
 // if users uses url?
 async function changeImageByUrl() {
 	result.style.display = "none";
-	if (inputURL.value.includes("base64")){
+	if (inputURL.value.includes("base64")) {
 		var blob = dataURItoBlob(inputURL.value);
 		imageDisplay.src = URL.createObjectURL(blob);
-	} else if (inputURL.value.match(/(jpg|jpeg|gif|png)((\?.*)$|$)/mg)	!= null) {
+	} else if (inputURL.value.match(/(jpg|jpeg|gif|png)((\?.*)$|$)/mg) != null) {
 		imageDisplay.src = inputURL.value;
 		fetch_img_url();
 	}
@@ -129,28 +127,26 @@ async function changeImageByUrl() {
 
 // convert base64 to normal url of an image
 // src: https://stackoverflow.com/questions/51416374/how-to-convert-base64-to-normal-image-url
-function dataURItoBlob(dataURI)
-{
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if(dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for(var i = 0; i < byteString.length; i++)
-    {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ia], {type: mimeString});
+function dataURItoBlob(dataURI) {
+	// convert base64/URLEncoded data component to raw binary data held in a string
+	var byteString;
+	if (dataURI.split(',')[0].indexOf('base64') >= 0)
+		byteString = atob(dataURI.split(',')[1]);
+	else
+		byteString = unescape(dataURI.split(',')[1]);
+	// separate out the mime component
+	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+	// write the bytes of the string to a typed array
+	var ia = new Uint8Array(byteString.length);
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+	return new Blob([ia], { type: mimeString });
 }
 
 // list of example images
 listImgs.getElementsByClassName("item").forEach(it => {
-	it.addEventListener("click", function(){
+	it.addEventListener("click", function () {
 		imageDisplay.src = it.firstElementChild.src;
 		result.style.display = "none";
 		mainImage.style.display = "block";
